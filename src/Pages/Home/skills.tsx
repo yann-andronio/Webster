3e754e6add28864a3117a7ac6fcb807a4f8ccbb7
IannisG10 <guerraiannis@gmail.com>
2024-09-16 14:10:37 +0200

    Ajout et stylisation de la section Langage de programamtion(Encore à ameliorer)

--- src/Pages/Home/skills.tsx
+++ src/Pages/Home/skills.tsx
@@ -1,40 +1,54 @@
 import { Fragment } from "react";
-import s from "../../css/team.module.css";
-import html from "../../assets/images/html.png";
-import css from "../../assets/images/css.png";
-import js from "../../assets/images/JS.png";
-import react from "../../assets/images/react.png";
+import s from "../../css/skill.module.css";
+import html5 from "../../assets/images/HTML5.png";
+import css3 from "../../assets/images/CSS3.png";
+import Js from "../../assets/images/javascript-64.png";
+import mongoDB from "../../assets/images/mongodb.png";
+import express from "../../assets/images/express (2).png";
+import ReactIco from "../../assets/images/react-native-96.png";
+import nodeIco from "../../assets/images/node-js.png";
 
 function Skill(){
 
     const langages = [
         {
-            logo: html,
-            name: "HTML"
+            logo: html5, name: "HTML"
         },
         {
-            logo: css,
-            name: "CSS"
+            logo: css3,name: "CSS"
         },
         {
-            logo: js,
-            name: "JavaScript"
+            logo: Js,name: "JavaScript"
         },
         {
-            logo: react,
-            name: "React"
+            logo: mongoDB,name: "MongoDB"
+        },
+        {
+            logo: express,name: "Express.js"
+        },
+        {
+            logo: ReactIco,name: "React.js"
+        },
+        {
+            logo: nodeIco,name: "Node.js"
         }
     ]
+
     return(
         <Fragment>
             <div className={s.BGimg}>
                 <h1 className={s.h1} style={{margin:25}}>LANGAGE DE PROGRAMMATION</h1>
-                <div>
+                <div className={s.skillContainer}>
                     {langages.map((item,index) =>
-                        <div key={index} style={{width:100}}>
+                        <div key={index} 
+                        style={{width:100,
+                                marginLeft: index >= 3 ? '100%':'0'
+                                }} 
+                        className={s.language}>
                             <img src={item.logo} alt={item.name} 
                                 className={s.langageImg}
                             />
+                            <div className={s.name}>{item.name}</div>
                         </div>    
                     )}
                 </div>
