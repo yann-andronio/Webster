ee18ca953dbcaf127c296e17ceec859d4e2b5405
AZCOM <145325495+yann-andronio@users.noreply.github.com>
2024-09-16 15:53:38 +0300

    modification de header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,12 +1,31 @@
-import { Fragment } from "react/jsx-runtime";
-import Navbar from "../navvbar/Navbar";
+import { Fragment } from 'react';
+import Navbar from '../navvbar/Navbar';
+import s from './header.module.css';
 
-
-export default function Header() {
+const Header: React.FC = () => {
   return (
-   <Fragment>
-    <Navbar/>
+    <Fragment>
+      <Navbar />
+      <div className={`${s.BoxprentBg}`}>
+        <div className={`${s.ChildBox} w-2/4  pl-32 pt-20`}>
 
-   </Fragment>
-  )
-}
+          <div className={s.text}>
+            <p className='text-3xl'>FREE</p>
+            <h1 className={`${s.name} text-6xl font-bold text-white `}>Webster</h1>
+            <p className='text-3xl ml-32'>LANCE</p>
+          </div>
+
+          <div className={s.description}>
+            <p className={` text-white  `} >Chez Webster, nous créons des sites web sur mesure qui propulsent votre entreprise vers le succès. Innovation, performance et design sont au cœur de tout ce que nous faisons pour offrir une expérience en ligne exceptionnelle.</p>
+
+          </div>
+        </div>
+
+
+      </div>
+
+    </Fragment>
+  );
+};
+
+export default Header;
--- src/components/navvbar/Navbar.tsx
+++ src/components/navvbar/Navbar.tsx
@@ -1,17 +1,14 @@
 import { useState } from 'react';
-import s from './navbar.module.css'; 
+import s from './navbar.module.css';
 import { FaBars } from 'react-icons/fa';
 import { RiHome3Line, RiServiceLine, RiMailLine } from 'react-icons/ri';
 import { FaProjectDiagram } from 'react-icons/fa';
 import { AiOutlineInfoCircle } from 'react-icons/ai';
 
-
-
 const Navbar: React.FC = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
   const [isClick, setIsClick] = useState<string>('Accueil');
 
- 
   const Scrolltopage = (id: string) => {
     const element = document.getElementById(id);
     if (element) {
@@ -22,14 +19,13 @@ const Navbar: React.FC = () => {
     }
   };
 
-
   const handleClick = (id: string) => {
     setIsClick(id);
     Scrolltopage(id);
   };
 
   return (
-    <nav className="bg-white border-gray-200 dark:bg-gray-900">
+    <nav className={s.nav}>
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
         <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
           Webmaster
@@ -46,7 +42,7 @@ const Navbar: React.FC = () => {
         </button>
 
         <div className={`${isOpen ? 'block' : 'hidden'} ${s.box_link} w-full md:block md:w-auto`} id="navbar-default">
-          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+          <ul className="font-medium flex flex-col p-4 md:p-0 mt-4  md:flex-row md:space-x-8 rtl:space-x-reverse">
             <li
               onClick={() => handleClick('Accueil')}
               className={`${s.link} ${s.colorLinkNavbar} ${isClick === 'Accueil' ? s.activeLink : s.inactiveLink}`}
@@ -56,28 +52,28 @@ const Navbar: React.FC = () => {
             </li>
             <li
               onClick={() => handleClick('Projets')}
-              className={`${s.link} ${s.colorLinkNavbar} ${isClick === 'Projets' ? s.activeLink : s.inactiveLink}`}
+              className={` ${isClick === 'Projets' ? s.activeLink : s.inactiveLink}`}
               id="Projets"
             >
               <FaProjectDiagram className="inline-block mr-2" /> Projets
             </li>
             <li
               onClick={() => handleClick('Services')}
-              className={`${s.link} ${s.colorLinkNavbar} ${isClick === 'Services' ? s.activeLink : s.inactiveLink}`}
+              className={` ${isClick === 'Services' ? s.activeLink : s.inactiveLink}`}
               id="Services"
             >
               <RiServiceLine className="inline-block mr-2" /> Services
             </li>
             <li
               onClick={() => handleClick('Apropos')}
-              className={`${s.link} ${s.colorLinkNavbar} ${isClick === 'Apropos' ? s.activeLink : s.inactiveLink}`}
+              className={` ${isClick === 'Apropos' ? s.activeLink : s.inactiveLink}`}
               id="Apropos"
             >
               <AiOutlineInfoCircle className="inline-block mr-2" /> A propos
             </li>
             <li
               onClick={() => handleClick('Contact')}
-              className={`${s.link} ${s.colorLinkNavbar} ${isClick === 'Contact' ? s.activeLink : s.inactiveLink}`}
+              className={`  ${isClick === 'Contact' ? s.activeLink : s.inactiveLink}`}
               id="Contact"
             >
               <RiMailLine className="inline-block mr-2" /> Contact
