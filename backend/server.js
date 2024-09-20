980b99939bb39ae3bb2fc21d7484355868d72575
IannisG10 <guerraiannis@gmail.com>
2024-09-20 14:55:06 +0200

    Creation du serveur backend

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,22 @@
 
 const express = require("express");
-const mongoose = require("mongoose");
+//const mongoose = require("mongoose");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 
 const app = express();
+const PORT = 3000;
+
+//Gestion des middleWare
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+
+app.listen(PORT,()=>{
+    try{
+        console.log(`Le serveur a demarré sur le port ${PORT}`);
+    }catch(err){
+        console.error(`Erreur lors du démarrage du serveur`);
+    }
+})
+
